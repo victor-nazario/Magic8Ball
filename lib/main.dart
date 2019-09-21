b8c59c2480f78e4b4487e07df0b9e0f45aa21955
Victor Nazario <victor.nazario@upr.edu>
2019-09-21 18:02:01 -0400

    Finished Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,38 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text("Ask Me Anything"
+            ),
+          ),
+          body: Ball(),
+        ),
       ),
     );
+
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: (){
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
